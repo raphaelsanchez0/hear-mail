8d4c14f8ae6dc9c6481f9049271647fe76635fab
Raphael Sanchez <sanchezraphael0@gmail.com>
2024-09-14 13:20:12 -0400

    feat: add settings nbutton

--- app/email/page.tsx
+++ app/email/page.tsx
@@ -6,7 +6,7 @@ import { Input } from "@/components/ui/input";
 import { ScrollArea } from "@/components/ui/scroll-area";
 import { Separator } from "@/components/ui/separator";
 import { Mail } from "@/utils/types";
-import { Inbox, Send, File, Trash2, Search } from "lucide-react";
+import { Inbox, Send, File, Trash2, Search, Settings } from "lucide-react";
 
 export default function EmailClientPage() {
   const exampleMail: Mail = {
@@ -22,11 +22,23 @@ export default function EmailClientPage() {
       {/* Sidebar */}
       <div className="w-44 bg-muted p-4 flex flex-col">
         <Button className="w-full mb-4">Compose</Button>
-        <nav className="space-y-2">
-          <NavButton name="Inbox" icon={<Inbox className="mr-2 h-4 w-4" />} />
-          <NavButton name="Sent" icon={<Send className="mr-2 h-4 w-4" />} />
-          <NavButton name="Drafts" icon={<File className="mr-2 h-4 w-4" />} />
-          <NavButton name="Trash" icon={<Trash2 className="mr-2 h-4 w-4" />} />
+        <nav className="space-y-2 flex flex-col h-full justify-between">
+          <div>
+            <NavButton name="Inbox" icon={<Inbox className="mr-2 h-4 w-4" />} />
+            <NavButton name="Sent" icon={<Send className="mr-2 h-4 w-4" />} />
+            <NavButton name="Drafts" icon={<File className="mr-2 h-4 w-4" />} />
+            <NavButton
+              name="Trash"
+              icon={<Trash2 className="mr-2 h-4 w-4" />}
+            />
+          </div>
+          <div>
+            <NavButton
+              name="Settings"
+              icon={<Settings className="mr-2 h-4 w-4" />}
+              href="/settings"
+            />
+          </div>
         </nav>
       </div>
 
--- components/nav-button/NavButton.tsx
+++ components/nav-button/NavButton.tsx
@@ -8,16 +8,19 @@ import { useSearchParams } from "next/navigation";
 interface NavButtonProps {
   name: string;
   icon: React.ReactNode;
+  href?: string;
 }
 
-export default function NavButton({ name, icon }: NavButtonProps) {
+export default function NavButton({ name, icon, href }: NavButtonProps) {
   const router = useRouter();
+
+  const onClickHref = href ? href : `/email?folder=${name.toLowerCase()}`;
 
   return (
     <Button
       variant="ghost"
       className="w-full justify-start"
-      onClick={() => router.push(`/email?folder=${name.toLowerCase()}`)}
+      onClick={() => router.push(onClickHref)}
     >
       {icon}
       {name}
