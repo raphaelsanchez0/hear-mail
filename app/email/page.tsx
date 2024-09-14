6b5f0dfbaedaccf110f5794aa1b23bc4298b25c0
Raphael Sanchez <sanchezraphael0@gmail.com>
2024-09-14 13:27:50 -0400

    feat: make sidebar a component

--- app/email/page.tsx
+++ app/email/page.tsx
@@ -1,6 +1,7 @@
 import EmailContent from "@/components/email-content/EmailContent";
 import EmailList from "@/components/email-list/EmailList";
 import NavButton from "@/components/nav-button/NavButton";
+import Sidebar from "@/components/sidebar/Sidebar";
 import { Button } from "@/components/ui/button";
 import { Input } from "@/components/ui/input";
 import { ScrollArea } from "@/components/ui/scroll-area";
@@ -20,27 +21,7 @@ export default function EmailClientPage() {
   return (
     <div className="flex h-screen bg-background">
       {/* Sidebar */}
-      <div className="w-44 bg-muted p-4 flex flex-col">
-        <Button className="w-full mb-4">Compose</Button>
-        <nav className="space-y-2 flex flex-col h-full justify-between">
-          <div>
-            <NavButton name="Inbox" icon={<Inbox className="mr-2 h-4 w-4" />} />
-            <NavButton name="Sent" icon={<Send className="mr-2 h-4 w-4" />} />
-            <NavButton name="Drafts" icon={<File className="mr-2 h-4 w-4" />} />
-            <NavButton
-              name="Trash"
-              icon={<Trash2 className="mr-2 h-4 w-4" />}
-            />
-          </div>
-          <div>
-            <NavButton
-              name="Settings"
-              icon={<Settings className="mr-2 h-4 w-4" />}
-              href="/settings"
-            />
-          </div>
-        </nav>
-      </div>
+      <Sidebar />
 
       {/* Email List */}
       <EmailList />
---
+++ components/sidebar/Sidebar.tsx
@@ -0,0 +1,27 @@
+import React from "react";
+import { Button } from "../ui/button";
+import NavButton from "../nav-button/NavButton";
+import { Inbox, Send, Settings, Trash2, File } from "lucide-react";
+
+export default function Sidebar() {
+  return (
+    <div className="w-44 bg-muted p-4 flex flex-col">
+      <Button className="w-full mb-4">Compose</Button>
+      <nav className="space-y-2 flex flex-col h-full justify-between">
+        <div>
+          <NavButton name="Inbox" icon={<Inbox className="mr-2 h-4 w-4" />} />
+          <NavButton name="Sent" icon={<Send className="mr-2 h-4 w-4" />} />
+          <NavButton name="Drafts" icon={<File className="mr-2 h-4 w-4" />} />
+          <NavButton name="Trash" icon={<Trash2 className="mr-2 h-4 w-4" />} />
+        </div>
+        <div>
+          <NavButton
+            name="Settings"
+            icon={<Settings className="mr-2 h-4 w-4" />}
+            href="/settings"
+          />
+        </div>
+      </nav>
+    </div>
+  );
+}
