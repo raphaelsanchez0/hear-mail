86b59e0e0f38c09a85657368468bdfdba29e7fa5
Raphael Sanchez <sanchezraphael0@gmail.com>
2024-09-14 14:04:04 -0400

    feat: authentication works

---
+++ app/settings/page.tsx
@@ -0,0 +1,32 @@
+"use client";
+import Sidebar from "@/components/sidebar/Sidebar";
+import { Button } from "@/components/ui/button";
+import {
+  Card,
+  CardContent,
+  CardFooter,
+  CardHeader,
+  CardTitle,
+} from "@/components/ui/card";
+import React from "react";
+import { signOut } from "next-auth/react";
+export default function Settings() {
+  return (
+    <div className="flex">
+      <Sidebar />
+      <div className="flex w-full">
+        <Card className="w-full basis-1/3">
+          <CardHeader>
+            <CardTitle className="text-2xl">Account</CardTitle>
+            <CardContent></CardContent>
+            <CardFooter>
+              <Button className="w-full" onClick={() => signOut()}>
+                Logout
+              </Button>
+            </CardFooter>
+          </CardHeader>
+        </Card>
+      </div>
+    </div>
+  );
+}
