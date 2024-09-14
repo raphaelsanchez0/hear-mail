0f229db436bb48c6531d51cc5c88cc1224c116cb
Raphael Sanchez <sanchezraphael0@gmail.com>
2024-09-14 15:19:46 -0400

    create compose dialog

--- components/compose-email-dialog/ComposeEmailDialog.tsx
+++ components/compose-email-dialog/ComposeEmailDialog.tsx
@@ -10,8 +10,33 @@ import {
 } from "@/components/ui/dialog";
 import { Input } from "@/components/ui/input";
 import { Label } from "@/components/ui/label";
+import { composeEmailSchema } from "@/utils/schemas";
+import { zodResolver } from "@hookform/resolvers/zod";
+import { useForm } from "react-hook-form";
+import { z } from "zod";
+import {
+  Form,
+  FormControl,
+  FormDescription,
+  FormField,
+  FormItem,
+  FormLabel,
+  FormMessage,
+} from "@/components/ui/form";
 
 export function ComposeEmailDialog() {
+  const form = useForm<z.infer<typeof composeEmailSchema>>({
+    resolver: zodResolver(composeEmailSchema),
+    defaultValues: {
+      recipientEmail: "",
+      subject: "",
+      body: "",
+    },
+  });
+
+  function onSubmit(values: z.infer<typeof composeEmailSchema>) {
+    console.log(values);
+  }
   return (
     <Dialog>
       <DialogTrigger asChild>
@@ -21,16 +46,61 @@ export function ComposeEmailDialog() {
         <DialogHeader>
           <DialogTitle>Compose</DialogTitle>
         </DialogHeader>
-        <Input type="email" placeholder="Recipient's Email" required />
-        <Input type="text" placeholder="Subject" required />
-        <textarea
-          className="w-full h-40 border border-gray-300 rounded p-2 mt-2"
-          placeholder="Message"
-          required
-        />
-        <Button type="submit" className="mt-2">
-          Send Email
-        </Button>
+        <Form {...form}>
+          <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-8">
+            <FormField
+              control={form.control}
+              name="recipientEmail"
+              render={({ field }) => (
+                <FormItem>
+                  <FormLabel>Email</FormLabel>
+                  <FormControl>
+                    <Input
+                      type="email"
+                      placeholder="Recipient's Email"
+                      {...field}
+                    />
+                  </FormControl>
+                  <FormMessage />
+                </FormItem>
+              )}
+            />
+            <FormField
+              control={form.control}
+              name="subject"
+              render={({ field }) => (
+                <FormItem>
+                  <FormLabel>Subject</FormLabel>
+                  <FormControl>
+                    <Input type="text" placeholder="Subject" {...field} />
+                  </FormControl>
+                  <FormMessage />
+                </FormItem>
+              )}
+            />
+            <FormField
+              control={form.control}
+              name="body"
+              render={({ field }) => (
+                <FormItem>
+                  <FormLabel>Body</FormLabel>
+                  <FormControl>
+                    <textarea
+                      className="w-full h-40 border border-gray-300 rounded p-2 mt-2"
+                      placeholder="Message"
+                      {...field}
+                    />
+                  </FormControl>
+                  <FormMessage />
+                </FormItem>
+              )}
+            />
+
+            <Button type="submit" className="mt-2">
+              Send Email
+            </Button>
+          </form>
+        </Form>
         <DialogFooter>
           {/* <Button type="submit">Save changes</Button> */}
         </DialogFooter>
