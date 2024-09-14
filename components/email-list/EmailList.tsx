a0c209e82491ef7d6b61bbe7f0a5623cf26fe928
Raphael Sanchez <sanchezraphael0@gmail.com>
2024-09-14 17:22:21 -0400

    gmail works

--- components/email-list/EmailList.tsx
+++ components/email-list/EmailList.tsx
@@ -1,10 +1,41 @@
 import { Search } from "lucide-react";
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { Input } from "../ui/input";
 import { ScrollArea } from "../ui/scroll-area";
 import ListEmail from "./ListEmail";
+import { useSession } from "next-auth/react";
+import { GmailMessage } from "@/utils/types";
+import { getEmails } from "@/api/gmail/gmail";
+import { getServerSession } from "next-auth";
+import { authConfig } from "@/lib/auth";
 
-export default function EmailList() {
+export default async function EmailList() {
+  const session = await getServerSession(authConfig);
+
+  const emails = await getEmails(session!.accessToken as string);
+
+  // // Function to fetch emails
+  // const fetchEmails = async (accessToken: string) => {
+  //   setLoading(true);
+  //   setError("");
+
+  //   try {
+  //     const emailList = await getEmails(accessToken);
+  //     setEmails(emailList);
+  //   } catch (err) {
+  //     setError(`Failed to fetch emails: ${(err as Error).message}`);
+  //   } finally {
+  //     setLoading(false);
+  //   }
+  // };
+
+  // // useEffect to handle the side-effect of fetching emails
+  // useEffect(() => {
+  //   if (status === "authenticated" && session?.accessToken) {
+  //     fetchEmails(session.accessToken as string); // Type assertion
+  //   }
+  // }, [status, session]);
+
   return (
     <div className="w-80 border-r">
       <div className="bg-background/95 p-4 backdrop-blur supports-[backdrop-filter]:bg-background/60">
@@ -16,14 +47,17 @@ export default function EmailList() {
         </form>
       </div>
       <ScrollArea className="h-[calc(100vh-5rem)] w-full px-4">
-        {[...Array(20)].map((_, i) => (
+        {emails.map((email) => (
+          <li key={email.id}>{email.snippet}</li>
+        ))}
+        {/* {[...Array(20)].map((_, i) => (
           <ListEmail
             key={i}
             sender="Sender Name"
             subject="Email Subject"
             body="Lorem ipsum dolor sit amet, consectetur adipiscing elit."
           />
-        ))}
+        ))}  */}
       </ScrollArea>
     </div>
   );
--- utils/types.ts
+++ utils/types.ts
@@ -5,3 +5,17 @@ export interface Mail {
   date: string;
   body: string;
 }
+
+export interface GmailMessage {
+  id: string;
+  threadId: string;
+  snippet: string;
+  // Add more fields as needed based on the Gmail API response
+}
+
+export interface GmailMessagesResponse {
+  messages: GmailMessage[];
+  nextPageToken?: string;
+  resultSizeEstimate?: number;
+  // Add more fields as needed
+}
