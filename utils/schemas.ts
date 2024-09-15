0920d45d8651373597fd43921e79caf82c68353d
alafshate <alafshate.m@gmail.com>
2024-09-15 02:38:45 -0400

    fuking works

--- utils/schemas.ts
+++ utils/schemas.ts
@@ -1,7 +1,10 @@
 import { z } from "zod";
 
 export const composeEmailSchema = z.object({
-  recipientEmail: z.string({ required_error: "Email is required" }).email(),
-  subject: z.string({ required_error: "Subject is required" }).max(255),
-  body: z.string({ required_error: "Subject is required" }).max(100000),
+  recipientEmail: z.string({ required_error: "Email is required" })
+    .email("Please enter a valid email address"),
+  subject: z.string({ required_error: "Subject is required" })
+    .max(255, "Subject can't be longer than 255 characters"),
+  body: z.string({ required_error: "Body is required" })
+    .max(100000, "Message body can't exceed 100,000 characters"),
 });
