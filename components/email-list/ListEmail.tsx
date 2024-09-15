450b588277fc5c503ea2a61ecbe2f46c4a7e6f83
Raphael Sanchez <sanchezraphael0@gmail.com>
2024-09-14 21:14:49 -0400

    feat: search param updates when email is clicked

--- components/email-list/ListEmail.tsx
+++ components/email-list/ListEmail.tsx
@@ -1,17 +1,28 @@
+"use client";
 import React from "react";
 import { Card } from "../ui/card";
 import { GmailMessage } from "@/utils/types";
 import { getSubject } from "@/utils/email/emailHelpers";
+import { useRouter } from "next/navigation";
 
 interface ListEmailProps {
   email: GmailMessage;
 }
 
 export default function ListEmail({ email }: ListEmailProps) {
-  console.log(email);
+  const params = new URLSearchParams();
+  const router = useRouter();
+  params.set("emailId", email.id);
+
+  const handleClick = () => {
+    router.push(`#${params.toString()}`);
+  };
 
   return (
-    <Card className="p-4 hover:bg-muted cursor-pointer max-w-full">
+    <Card
+      className="p-4 hover:bg-muted cursor-pointer max-w-full"
+      onClick={handleClick}
+    >
       <h3 className="font-semibold text-md w-full">{email.senderName}</h3>
       <p className="text-md overflow-hidden whitespace-nowrap">
         {getSubject(email)}
