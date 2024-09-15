87e69cb2a09c5e0c8e523bbdfd80d6031cd25a31
YazanShaw <yazan.nashama@gmail.com>
2024-09-15 03:38:08 -0400

    added summarizer

---
+++ components/ai-interpreter/summarize.tsx
@@ -0,0 +1,36 @@
+"use client"
+import React, { useState } from "react";
+import OpenAI from "openai";
+import { Button } from "@/components/ui/button";
+
+const openAi = new OpenAI({
+    apiKey:process.env.NEXT_PUBLIC_OPENAI_API_KEY,
+    dangerouslyAllowBrowser:true
+})
+
+export function Summarization({ inputText} :{inputText:string}) {
+    const [response, setResponse] = useState('')
+
+    const handleClick = async() =>{
+        const params:OpenAI.Chat.ChatCompletionCreateParams =
+        {
+            messages: [{role: 'user', content: "Summarize the following: " + inputText }],
+            model: 'gpt-3.5-turbo'
+        };
+        const chatCompletion: OpenAI.Chat.ChatCompletion = await openAi.chat.completions.create(params);
+        
+        const chatGPTResponse = chatCompletion.choices[0].message.content
+        setResponse(chatGPTResponse!)
+    }
+   
+    return(
+        <>
+        <div className="flex">
+            
+            <Button onClick={()=>handleClick()}/>
+                
+        </div>
+        {response}
+        </>
+    )
+}
